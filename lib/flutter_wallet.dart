2739a7f085131d4ce6b820e4a439fc354d2f838e
Mahmoud Tolba <78425511+Mahmoud-t0lba@users.noreply.github.com>
2025-02-24 16:38:31 +0200

    update

--- lib/flutter_wallet.dart
+++ lib/flutter_wallet.dart
@@ -30,20 +30,18 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
  */
 import 'dart:async';
+
 import 'package:flutter/services.dart';
-import 'package:meta/meta.dart';
 
 class FlutterWallet {
-  static const MethodChannel _channel =
-      const MethodChannel('com.vico-aguado.flutter/wallet');
+  static const MethodChannel _channel = const MethodChannel('com.vico-aguado.flutter/wallet');
 
   /// Returns bool value if iOS can covert one pkpass file
   ///
   /// - parameters:
-  ///    - pkpass: List with <int> values from pkpass file 
-  static Future<bool> addPass({@required List<int> pkpass}) async {
-    final bool result = await _channel
-        .invokeMethod('addWalletPass', <String, dynamic>{'pkpass': pkpass});
+  ///    - pkpass: List with <int> values from pkpass file
+  static Future<bool> addPass({required List<int> pkpass}) async {
+    final bool result = await _channel.invokeMethod('addWalletPass', <String, dynamic>{'pkpass': pkpass});
     return result;
   }
 }
